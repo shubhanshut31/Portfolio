dc4de6bdabed4deebab9e8cda1ca4f1b89ff3669
shubhanshu Tiwari <149867878+shubhanshut31@users.noreply.github.com>
2024-11-24 19:10:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", 
+        "Full Stack Developer", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    }
+)
+    
